72534a1ca44d57a0168a28e6f26d34dc954e1ea6
Mudit0704 <maheshwari7mudit@gmail.com>
2023-04-12 17:21:42 -0400

    Added market admins middle ware implementation

---
+++ controllers/market/admins.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    "_id": 1,
+    "name": "Admin 1",
+    "type": "Admin",
+    "sub_type": "MARKET_ADMIN",
+    "email": "a1@nu-one.com",
+    "password": "a1",
+    "contact": "111-111-1111",
+    "address": "1111 1st St",
+    "image": "logo192.png"
+  },
+  {
+    "_id": 2,
+    "name": "Admin 2",
+    "type": "Admin",
+    "sub_type": "MARKET_ADMIN",
+    "email": "a2@nu-one.com",
+    "password": "a2",
+    "contact": "222-222-2222",
+    "address": "2222 2nd St",
+    "image": "logo192.png"
+  },
+  {
+    "_id": 3,
+    "name": "User 1",
+    "type": "User",
+    "sub_type": "",
+    "email": "u1@nu-one.com",
+    "password": "u1",
+    "contact": "333-333-3333",
+    "address": "3333 3rd St",
+    "image": "logo192.png"
+  },
+  {
+    "_id": 4,
+    "name": "Admin 3",
+    "type": "Admin",
+    "sub_type": "FOOD_ADMIN",
+    "email": "a3@nu-one.com",
+    "password": "a3",
+    "contact": "444-444-4444",
+    "address": "4444 4th St",
+    "image": "logo192.png"
+  }
+]
