c3bdd0c874763ee90e80119a9f1ca736df22f4ca
Mudit0704 <maheshwari7mudit@gmail.com>
2023-04-12 15:34:11 -0400

    Added products middle ware implementation

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+import ProductsController from "./controllers/market/products-controller.js";
+
 const app = express();
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(cors());
+app.use(express.json());
+ProductsController(app);
 app.listen(4000);
---
+++ controllers/market/products.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    "_id": 1,
+    "seller_ids": [1, 2],
+    "name": "Product 1",
+    "brand": "Brand 1",
+    "description": "Description 1",
+    "price": 1.00,
+    "quantity": 1,
+    "image": "logo192.png",
+    "rating": 1
+  },
+  {
+    "_id": 2,
+    "seller_ids": 2,
+    "name": "Product 2",
+    "brand": "Brand 2",
+    "description": "Description 2",
+    "price": 2.00,
+    "quantity": 2,
+    "image": "logo192.png",
+    "rating": 2
+  }
+]
