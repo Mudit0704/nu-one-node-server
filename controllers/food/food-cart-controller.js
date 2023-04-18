cbd3e54bdcab90da467b030df725582433dd2b87
add19 <aadish.deshpande@gmail.com>
2023-04-18 18:33:01 -0400

    cart changes complete

--- controllers/food/food-cart-controller.js
+++ controllers/food/food-cart-controller.js
@@ -2,19 +2,22 @@ import * as foodCartDao from "./food-cart-dao.js";
 let user_id = "643855e4584213571e02854c"
 
 const findFoodCartItems = async (req, res) => {
+  const user_id = req.params.userId;
   const cartArray = await foodCartDao.findFoodCartByUserId(user_id);
   res.json(cartArray[0].items);
 }
 
 const deleteFoodCartItems = async (req, res) => {
   const itemIdToDelete = req.params.itemId;
+  const user_id = req.params.userId;
   const status = await foodCartDao.deleteFoodCartItem(user_id, itemIdToDelete);
   res.json(status);
 }
 
 const addToFoodCart = async (req, res) => {
-  const exists = await foodCartDao.findFoodCartExists(user_id);
+  const user_id = req.body.user_id;
 
+  const exists = await foodCartDao.findFoodCartExists(user_id);
   if (!exists) {
     await foodCartDao.createFoodCart(user_id);
     await foodCartDao.addFoodCartItem(user_id, req.body);
@@ -38,13 +41,14 @@ const addToFoodCart = async (req, res) => {
 }
 
 const clearFoodCart = async (req, res) => {
+  const user_id = req.params.userId;
   const status = await foodCartDao.clearFoodCart(user_id);
   res.json(status);
 }
 
 export default (app) => {
-  app.get("/api/foodCart", findFoodCartItems);
-  app.delete("/api/foodCart/:itemId", deleteFoodCartItems);
-  app.delete("/api/foodCart/", clearFoodCart);
+  app.get("/api/foodCart/:userId", findFoodCartItems);
+  app.delete("/api/foodCart/:itemId/:userId", deleteFoodCartItems);
+  app.delete("/api/foodCart/:userId", clearFoodCart); // -> also need to pass the user id of the cart
   app.post("/api/foodCart/", addToFoodCart);
 }
--- controllers/food/food-restaurant-dao.js
+++ controllers/food/food-restaurant-dao.js
@@ -10,29 +10,7 @@ export const findRestaurantsByOwnerId = (ownerId) => {
 
 export const addToRestaurantMenu = (restaurantId, menuItem) => foodRestaurantModel.updateOne({_id: restaurantId}, {$push: {menu_items: menuItem}});
 
-export const updateRestaurantMenuItem = (restaurantId, menuItemId, updates) => {
-  console.log(updates)
-  console.log(restaurantId)
-  console.log(menuItemId)
-  return foodRestaurantModel.updateOne(
-      {_id: restaurantId, "menu_items._id": menuItemId},
-      {$set: {"menu_items.$": updates}})
-};
-
-// db.foodRestaurants.updateOne({_id: ObjectId('643dc0ac1b29be2ae5e43f27'),
-//   "menu_items._id": ObjectId('643e045fc02211e62bba1142')},
-//     {$set: {"menu_items.$":
-//             {
-//       _id: '643e045fc02211e62bba1142',
-//       itemName: 'Food Item 1 Updated',
-//       restaurant_id: '643dc0ac1b29be2ae5e43f27',
-//       itemDescription: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
-//       price: 9.99,
-//       image: 'logo192.png',
-//       calories: 300,
-//       categories: [ '643acb105f33ffcba6677208' ]
-//     }
-//   }})
+export const updateRestaurantMenuItem = (restaurantId, menuItemId, updates) => foodRestaurantModel.updateOne({_id: restaurantId, "menu_items._id": menuItemId}, {$set: {"menu_items.$": updates}});
 
 export const deleteRestaurantFoodItem = (restaurantId, menuItemId) => foodRestaurantModel.updateOne({_id: restaurantId}, {$pull: {menu_items : {_id : menuItemId}}});
 
