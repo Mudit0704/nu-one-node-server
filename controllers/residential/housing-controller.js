595b497ffeb1cd4828863e3ffd2205b00175b070
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2023-04-23 16:11:05 -0400

    Final Changes made

--- controllers/residential/housing-controller.js
+++ controllers/residential/housing-controller.js
@@ -1,11 +1,23 @@
 import * as housingDao from "./housing-dao.js"
 import {findHouseById} from "./housing-dao.js";
+import multer from "multer";
+import fs from "fs";
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/residentialImages")
+    },
+    filename: function (req, file, cb) {
+        cb(null,file.originalname)
+    }
+});
+
+const upload = multer({storage: storage});
 
 const HousingController = (app) => {
     app.get('/api/houses', findHouses)
     app.get('/api/houses/:uid', findUserById);
-    app.post('/api/houses', createHouse);
+    app.post('/api/houses', upload.single('image'), createHouse);
 }
 
 const findHouses = async (req, res) => {
@@ -14,10 +26,26 @@ const findHouses = async (req, res) => {
 }
 
 const createHouse = async (req, res) => {
-    const newHouse = req.body;
+    const data = fs.readFileSync("./public/residentialImages/" + req.file.originalname);
+    const contentType = "image/png";
+
+    const newHouse = {
+        houseName: req.body.name,
+        HouseDescription: req.body.description,
+        rent: req.body.rent,
+        streetAddress: req.body.streetAddress,
+        bedrooms: req.body.bedroom,
+        image: {
+            data: data,
+            contentType: contentType
+        },
+        bathrooms: req.body.bathroom
+    }
+
     newHouse._id = (new Date()).getTime() + '';
     const insertedHouse = await housingDao
         .createHouses(newHouse);
+
     res.json(insertedHouse);
 }
 
